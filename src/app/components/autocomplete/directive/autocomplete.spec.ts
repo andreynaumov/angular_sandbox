fe42ba16be3bc1061c44ed7806128aa246694045
example <you@example.com>
2025-06-23 23:40:22 +0300

    create autocomplete directive

---
+++ src/app/components/autocomplete/directive/autocomplete.spec.ts
@@ -0,0 +1,8 @@
+import { Autocomplete } from './autocomplete';
+
+describe('Autocomplete', () => {
+  it('should create an instance', () => {
+    const directive = new Autocomplete();
+    expect(directive).toBeTruthy();
+  });
+});
