9daccc465c9ecaa3cb1aa0182d3311e86693e0b2
example <you@example.com>
2025-06-23 23:37:26 +0300

    add example for material autocomplete

--- src/app/pages/example-autocomplete/example-autocomplete.ts
+++ src/app/pages/example-autocomplete/example-autocomplete.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { Autocomplete } from '../../components/autocomplete/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { AbstractControl, FormControl, ReactiveFormsModule, ValidationErrors } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { SelectOption } from '../../components/form-builder/shared/types/form-config';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-example-autocomplete',
-  imports: [Autocomplete],
+  imports: [MatFormFieldModule, MatInputModule, MatAutocompleteModule, ReactiveFormsModule, MatButtonModule],
   templateUrl: './example-autocomplete.html',
   styleUrl: './example-autocomplete.scss',
 })
-export class ExampleAutocomplete {}
+export class ExampleAutocomplete {
+  public readonly addressControlMaterial = new FormControl<string | { value: string }>('', [addressValidator]);
+
+  public readonly options = signal<SelectOption<any>[]>([]);
+
+  public readonly displayFn = (address: { value: string } | string) => (address && typeof address === 'object' ? address.value : address);
+
+  public ngOnInit() {
+    from(fetch('dadata-options.json').then((r) => r.json())).subscribe((response) => {
+      // console.log(response.options);
+      this.options.set(response.options);
+    });
+
+    this.addressControlMaterial.valueChanges.subscribe((value) => {
+      // console.log(value);
+    });
+  }
+
+  public updateControlValue() {
+    this.addressControlMaterial.setValue({ value: 'foo' });
+  }
+}
+
+function addressValidator(control: AbstractControl): ValidationErrors | null {
+  if (typeof control.value === 'string') {
+    return {
+      invalidAddress: true,
+    };
+  }
+
+  return null;
+}
