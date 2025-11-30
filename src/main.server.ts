11fc4b17cb5559e0679296d20d9f5057f4aeb242
An <andrey.naumoff@gmail.com>
2025-11-30 21:15:51 +0300

    fixes after update angular

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,9 +1,7 @@
-import { provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication, BootstrapContext } from '@angular/platform-browser';
 import { App } from './app/app';
 import { config } from './app/app.config.server';
 
-const bootstrap = (context: BootstrapContext) =>
-  bootstrapApplication(App, { ...config, providers: [provideZoneChangeDetection(), ...config.providers] }, context);
+const bootstrap = (context: BootstrapContext) => bootstrapApplication(App, config, context);
 
 export default bootstrap;
